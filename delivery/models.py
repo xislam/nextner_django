6a579d2b886542a72e6a618927f8d9ca6af9c0de
islam <nudinovislam4@gmail.com>
2022-02-23 22:33:47 +0600

    Написаны модели базы данных 1# https://github.com/xislam/nextner_django/issues/1#issue-1147805254

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    """Тип продукта"""
+    name = models.CharField(verbose_name="Наименование типа", max_length=125)
+
+    def __str__(self):
+        return self.name
+
+
+class Delivery(models.Model):
+    """Данные товара для доставки"""
+    product_name = models.CharField(verbose_name='Имя товара', max_length=125)
+    product_type = models.ManyToManyField(ProductType, verbose_name="Тип товака")
+    delivery_date = models.DateField(verbose_name='Дата доставки')
+    file = models.FileField(verbose_name="Файл", upload_to="delivery")
+
+    def __str__(self):
+        return self.product_name
+
+
+class MultipleAddresses(models.Model):
+    """Множественных адресов пунктов выдачи"""
+    delivery = models.ForeignKey(Delivery, verbose_name='Множественных Адресов Указанных Выдачи',
+                                 on_delete=models.CASCADE)
+    address = models.TextField(verbose_name="Наимнование адриса")
+
+    def __str__(self):
+        return self.delivery.product_name
+
+
