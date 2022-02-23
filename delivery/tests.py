b6094499cd23a3207d7f2a26df39c32bb7b93da0
islam <nudinovislam4@gmail.com>
2022-02-23 23:59:13 +0600

    написаны тесты для проверки модели на данный момент по задаче #1 https://github.com/xislam/nextner_django/issues/1#issue-1147805254

--- delivery/tests.py
+++ delivery/tests.py
@@ -1,3 +1,22 @@
+import datetime
+
+from django.core.files import File
 from django.test import TestCase
 
-# Create your tests here.
+from delivery.models import *
+import mock
+
+
+class TestAppModels(TestCase):
+
+    def test_model_str(self):
+        file_mock = mock.MagicMock(spec=File)
+        file_mock.name = 'test.pdf'
+        product_type_name = ProductType.objects.create(name="Экспорт")
+        delivery_test = Delivery.objects.create(product_name='Машина',
+                                                delivery_date=datetime.datetime.now().date(), file=file_mock)
+        delivery_test.product_type.set([product_type_name.pk])
+        multiple_addresses = MultipleAddresses.objects.create(delivery=delivery_test, address='тест')
+        self.assertEqual(str(product_type_name), "Экспорт")
+        self.assertEqual(str(delivery_test), 'Машина')
+        self.assertEqual(str(multiple_addresses), 'Машина')
